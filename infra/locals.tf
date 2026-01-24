7f7e0fc28e9dfee0195d0854223a4a92cb441aa6
vimallinuxworld13 <vdaga@lwindia.com>
2026-01-24 17:49:48 +0530

    Update locals.tf

--- infra/locals.tf
+++ infra/locals.tf
@@ -8,7 +8,7 @@ locals {
     apt-get install -y nginx
     systemctl enable nginx
     systemctl start nginx
-    echo "<h1>${local.name_prefix} - $(hostname)</h1>" > /var/www/html/index.html
+    echo "Vimal - v1 - <h1>${local.name_prefix} - $(hostname)</h1>" > /var/www/html/index.html
   EOT
 }
 
